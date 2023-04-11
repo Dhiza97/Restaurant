363aa3c70545ebf301c6f221493132b098045bb4
Joseph Garba <josephgarba97@gmail.com>
2023-04-11 18:14:02 +0100

    setting up server

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const ejs = require('ejs')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
 
 
 
